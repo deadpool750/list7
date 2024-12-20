92acbc97858809ec6d8896ca30e59cdcf4364f12
dpl750 <278435@student.pwr.edu.pl>
2024-12-20 10:19:58 +0100

    correctly renamed files testing out git

--- app/src/main/java/com/example/list7/firebase/User.kt
+++ app/src/main/java/com/example/list7/firebase/User.kt
@@ -1,22 +1,20 @@
 package com.example.list7.firebase
 
 data class User(
-    val id: String = "",
     val name: String = "",
     val surname: String = "",
     val email: String = "",
     val phoneNumber: String = "",
-    val address: Map<String, String> = mapOf(),
-){
-    companion object{
-        fun fromMap(map: Map<String, Any?>): User{
+    val address: String = ""
+) {
+    companion object {
+        fun fromMap(data: Map<String, Any?>): User {
             return User(
-                id = map["id"] as? String ?: "",
-                name = map["name"] as? String ?: "",
-                surname = map["surname"] as? String ?: "",
-                email = map["email"] as? String ?: "",
-                phoneNumber = map["phoneNumber"] as? String ?: "",
-                address = map["address"] as? Map<String, String> ?: mapOf()
+                name = data["name"] as? String ?: "",
+                surname = data["surname"] as? String ?: "",
+                email = data["email"] as? String ?: "",
+                phoneNumber = data["phoneNumber"] as? String ?: "",
+                address = data["address"] as? String ?: ""
             )
         }
     }
